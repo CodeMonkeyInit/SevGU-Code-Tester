71af65ff8de30529eac77ff1f08d914abbfbce08
Victor <43318916+pDopnex@users.noreply.github.com>
2018-10-18 15:58:32 +0300

    Добавил пустое веб-приложение

---
+++ src/CodeTester/CodeTester.WebApp/Source/Startup.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CodeTester.WebApp.Source
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services) { }
+
+        public void Configure(IApplicationBuilder builder, IHostingEnvironment environment)
+        {
+            if (environment.IsDevelopment())
+                builder.UseDeveloperExceptionPage();
+
+            builder.Run(Handle);
+        }
+
+        private static async Task Handle(HttpContext context) =>
+            await GetHttpResponse(context).WriteAsync("code-tester");
+
+        private static HttpResponse GetHttpResponse(HttpContext context) =>
+            context.Response;
+    }
+}
